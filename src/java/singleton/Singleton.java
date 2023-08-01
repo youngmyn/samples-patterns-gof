951cee09040e0b9c8a5950453b93b6e5f193e5c3
YoungMYN <Shyliko315on@gmail.com>
2023-08-01 16:27:20 +0700

    test commit

---
+++ src/java/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class Singleton {
+    private static Singleton INSTANCE;
+    private Singleton(){}
+    public static Singleton getInstance(){
+        if(INSTANCE==null){
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+}
+
+
