96f2fc3efcddd2f8d62169300506db176fa8bb27
YoungMYN <Shyliko315on@gmail.com>
2023-08-11 15:12:37 +0700

    iterator added

---
+++ src/java/iterator/concreteAggregates/MyContainer1.java
@@ -0,0 +1,32 @@
+package iterator.concreteAggregates;
+
+import iterator.Aggregate;
+import iterator.Iterator;
+
+public class MyContainer1 implements Aggregate {
+    private String[] names = new String[5];
+    public MyContainer1(){
+        names[0] = "Gleb";
+        names[1] = "Nastya";
+        names[2] = "Anya";
+        names[3] = "Yan";
+        names[4] = "John";
+    }
+    @Override
+    public Iterator getIterator() {
+        return new Iter();
+    }
+    private class Iter implements Iterator{
+        int cursor=0;
+        @Override
+        public boolean hasNext() {
+            return names.length > cursor;
+        }
+
+        @Override
+        public Object next() {
+            return names[cursor++];
+        }
+
+    }
+}
